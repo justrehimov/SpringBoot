1fedf2f51a79b191184a26de01adafe8ff2598b7
nur00103 <nur1398@mail.ru>
2022-08-31 01:31:09 +0400

    ....

--- src/main/java/az/spring/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/az/spring/springboot/service/impl/UserServiceImpl.java
@@ -30,4 +30,27 @@ public class UserServiceImpl implements UserService {
        Optional<User> optionalUser =  userRepository.findById(id);
        return optionalUser.orElseThrow(()->new IllegalArgumentException("User not found"));
     }
+    @Override
+    public User update(User user, Long id) {
+        Optional<User> savedUser=userRepository.findById(id);
+        if (savedUser.isPresent()){
+            savedUser.get().setFirstName(user.getFirstName());
+            savedUser.get().setLastName(user.getLastName());
+            savedUser.get().setAge(user.getAge());
+            return userRepository.save(savedUser.get());
+        }else {
+            return savedUser.get();
+        }
+    }
+
+    @Override
+    public User delete(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+            return user.get();
+        } else {
+            return null;
+        }
+    }
 }
